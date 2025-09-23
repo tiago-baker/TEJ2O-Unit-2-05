5a295e57a248ecd77e01d501ecbf4afc72b6c178
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-09-23 13:23:31 -0400

    did most of the code

--- main.ts
+++ main.ts
@@ -9,3 +9,19 @@ basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // comment
+let TemperatureKelvin: number
+let TemperatureValue: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // get temperature value
+    TemperatureKelvin = input.temperature()
+    TemperatureValue = (TemperatureKelvin / 273) + 21
+    TemperatureValue = Math.round(TemperatureValue)
+
+    // output temperature
+    basic.clearScreen()
+    basic.showString('The Temperature is: ' + TemperatureValue.toString() + 'K.')
+})
